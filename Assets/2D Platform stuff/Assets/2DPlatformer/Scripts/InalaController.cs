9b4f5a2e801dadb2df21ee7daac79789f4ae6a1b
Jacob Queen <jacobsq@protonmail.com>
2019-01-25 07:54:26 -0700

    ..

---
+++ Assets/2D Platform stuff/Assets/2DPlatformer/Scripts/InalaController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InalaController : PhysicsObject 
+{
+	private GameObject player;
+
+	private float speed = 3.0f;
+	private float offset = 5.0f;
+	
+	void Awake () 
+	{
+		player = GameObject.Find("Player");
+		rb2d = GetComponent<Rigidbody2D>();
+		contactFilter.useTriggers = false;
+		contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+		contactFilter.useLayerMask = true;
+		gravityModifier = 0.0f;
+	}
+	
+	
+	protected override void ComputeVelocity()
+	{
+		Vector2 move = Vector2.zero;
+
+		if (player.transform.position.x - offset > gameObject.transform.position.x)
+		{
+			move.x = 1;
+		}
+		else if (player.transform.position.x + offset > gameObject.transform.position.x)
+		{
+			move.x = -1;
+		}
+
+		targetVelocity = move * speed;
+	}
+
+
+}
